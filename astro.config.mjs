cf9f26a378bfb936b9871a5878834ccae8f43c28
Gummientchen <steff.freak@gmail.com>
2023-05-09 17:21:29 +0200

    added github actions for autodeploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import compress from "astro-compress";
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()]
+  site: 'https://gummientchen.github.io',
+  base: '/gummientchen-net',
+  integrations: [compress()],
 });
